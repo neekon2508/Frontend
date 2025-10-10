0f839d896363fc48c6901d07b1c0fb4fbd0ec0ae
Nguyen Trung Nghia <nghia250803@lgcns.com>
2025-10-10 11:35:57 +0700

    Create Ultimate React Course

---
+++ 3.React/UltimateReactCourse/Practice/07-usepopcorn/src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import StarRating from "./StarRating.jsx";
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars </p>
+//     </div>
+//   );
+// }
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
