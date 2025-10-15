9a49de84c8e2305388d15aefde344c2d61422868
Nguyen Trung Nghia <127302636+neekon2508@users.noreply.github.com>
2025-10-16 06:56:14 +0700

    Update

--- 3.React/UltimateReactCourse/Practice/11-worldwise/src/pages/Homepage.jsx
+++ 3.React/UltimateReactCourse/Practice/11-worldwise/src/pages/Homepage.jsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 export default function HomePage() {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Worldwise</h1>
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
 }
