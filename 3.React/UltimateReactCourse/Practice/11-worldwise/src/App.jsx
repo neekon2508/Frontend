f5524028381bf238c1ff1edaf6bebdd5d8f7f369
Neekon <nghia250803@gmail.com>
2025-10-15 19:05:01 +0700

    Ultimate React Course: update section 17

--- 3.React/UltimateReactCourse/Practice/11-worldwise/src/App.jsx
+++ 3.React/UltimateReactCourse/Practice/11-worldwise/src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
-import DateCounter from "./DateCounter";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 export default function App() {
   return (
     <div>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
