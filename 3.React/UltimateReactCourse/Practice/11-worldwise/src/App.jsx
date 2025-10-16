aa0a96a435f97ef9af2bf63aec741be78263b8ff
Neekon <nghia250803@gmail.com>
2025-10-16 18:55:59 +0700

    Update

--- 3.React/UltimateReactCourse/Practice/11-worldwise/src/App.jsx
+++ 3.React/UltimateReactCourse/Practice/11-worldwise/src/App.jsx
@@ -3,6 +3,7 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 export default function App() {
   return (
     <div>
@@ -12,6 +13,7 @@ export default function App() {
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="*" element={<PageNotFound />} />
+          <Route path="app" element={<AppLayout />} />
         </Routes>
       </BrowserRouter>
     </div>
--- 3.React/UltimateReactCourse/Practice/11-worldwise/src/pages/PageNotFound.jsx
+++ 3.React/UltimateReactCourse/Practice/11-worldwise/src/pages/PageNotFound.jsx
@@ -1,9 +1,7 @@
-import PageNav from "../components/PageNav";
-
 export default function PageNotFound() {
   return (
     <div>
-      <PageNav /> found :(
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
