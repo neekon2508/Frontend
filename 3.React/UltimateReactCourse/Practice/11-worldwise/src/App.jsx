dcbecc20a7f4b7d49e042c6ae2d0110adbf02894
Neekon <nghia250803@gmail.com>
2025-10-17 16:23:08 +0700

    Update

--- 3.React/UltimateReactCourse/Practice/11-worldwise/src/App.jsx
+++ 3.React/UltimateReactCourse/Practice/11-worldwise/src/App.jsx
@@ -4,16 +4,23 @@ import Pricing from "./pages/Pricing";
 import HomePage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 export default function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route index element={<HomePage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>LIST</p>} />
+            <Route path="cities" element={<p>List of cities</p>} />
+            <Route path="countries" element={<p>Countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
           <Route path="*" element={<PageNotFound />} />
-          <Route path="app" element={<AppLayout />} />
         </Routes>
       </BrowserRouter>
     </div>
--- 3.React/UltimateReactCourse/Practice/11-worldwise/src/pages/AppLayout.jsx
+++ 3.React/UltimateReactCourse/Practice/11-worldwise/src/pages/AppLayout.jsx
@@ -1,15 +1,12 @@
-// import Map from "../components/Map";
-// import Sidebar from "../components/Sidebar";
-// import User from "../components/User";
-
+import Map from "../components/Map";
+import SideBar from "../components/SideBar";
 import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
-      {/* <Sidebar />
+      <SideBar />
       <Map />
-      <User /> */}
     </div>
   );
 }
